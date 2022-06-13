fcf6705b600ce03d6df92d99309a86f5719a7440
RMjessica <jeess.rm1@gmail.com>
2022-06-13 15:33:13 +0000

    Initial commit
    -Set up basic structure
    -Give some styling

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaHeart } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
+
 // import { ReactComponent as Yoda } from "../..img/yoda.jpg";
 
 
 export const Card = (props) => {
+
   return (
     <div className="">
       <h1></h1>
@@ -12,6 +14,7 @@ export const Card = (props) => {
 
         <img src="" className="card-img-top img-fluid" alt="pic"/>
         <div className="card-body">
+          
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Gender: {}</p>
           <p className="card-text">Hair-color: {}</p>
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer m-2 py-5 text-center">
+		<p><a className="text-dark" href="https://www.starwars.com/">StarWars Oficial Site</a></p>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a className="text-dark" href="https://github.com/RMjessica">Jeess</a>
