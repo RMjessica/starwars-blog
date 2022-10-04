306508abf3d11d3eed39cac9f4c4935665deef0a
RMjessica <jeess.rm1@gmail.com>
2022-10-04 19:45:51 +0000

    Edit dropdown style & responsiveness

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -17,51 +17,73 @@ export const Navbar = () => {
 
 
 	return (
-		<nav className="navbar navbar-expand-lg m-0 p-3 d-flex justify-content-between">
-			<Link to="/">
-				<div className="navbar-brand mb-0 h1">
-					<img src={starwarslogo2} alt="star wars" width="115" height="60" className="d-inline-block ms-3"/>
-				</div>
-			</Link>
-			<button className="navbar-toggler border-none me-3" id="button-background" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> 
-				<FaGripLines className="text-warning navbar-toggler-icon" size={30}/>
-			</button>
-
-			<div className="collapse navbar-collapse" id="navbarNav">
-				<div className="navbar-nav me-5">
-					
-					<Link to="/favorites" className="nav-item dropdown-toggle text-decoration-none font-monospace fw-light" style={{color: "rgb(214, 201, 63)"}} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-						<h3 className="mt-2" style={{color: "rgb(214, 201, 63)"}}>FAVORITES</h3>
-					</Link>
-
-					<ul className="dropdown-menu dropdown-menu-dark dropdown-menu-end me-5 dropdown" style={{color: "rgb(214, 201, 63)"}} aria-labelledby="dropdownMenuButton1">
-					{store.hasFavorites ? data_types.map((data_type) => {
-						return (
-							store.favorites[data_type].map((index) => {
-								return (
-									<li key={uuidv4()} className="d-flex" style={{color: "rgb(214, 201, 63)"}}>
-										<Link to="/detail" className="dropdown-item" style={{color: "rgb(214, 201, 63)"}} title="Click for details">
-											{store.results[data_type][index].name}
-										</Link>
-										<button className="btn">
-											<FaTrashAlt size={15} title="Delete" style={{color: "rgb(214, 201, 63)"}} className="mb-2" onClick={() => deleteFavorite(data_type, index)}/>
-										</button>
-									</li>
-								)
-							})
-						)
-					})  : <li className="text-center m-1">No favorites yet</li> }
-						<li><hr className="mt-1 mb-2 dropdown-divider mx-2"></hr></li>
-
-						<li className="text-center m-1">
-							<Link to="/favorites" className="text-decoration-none" style={{color: "rgb(214, 201, 63)"}}>
-									Go to favorites
-							</Link>
+		<nav className="navbar navbar-expand-lg ms-auto px-4 py-3">
+			<div className="container-fluid">
+				
+				<Link to="/" className="navbar-brand ">
+					<img src={starwarslogo2} alt="star wars" width="115" height="60" className=""/>
+				</Link>
+				
+				<button 
+					className="navbar-toggler" 
+					type="button" 
+					data-bs-toggle="collapse" 
+					data-bs-target="#navbarNavDropdown" 
+					aria-controls="navbarNavDropdown" 
+					aria-expanded="false" 
+					aria-label="Toggle navigation"
+				>
+					<FaGripLines className="text-warning navbar-toggler-icon" size={30}/>
+				</button>
+				
+				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul className="navbar-nav">
+						
+						<li className="nav-item">
+							<a className="nav-link active visually-hidden" aria-current="page">Home</a>
 						</li>
+						
+						<li className="nav-item dropdown">
+							<Link 	
+								to="/favorites" 
+								/* title="Click to go to favorites" */
+								className="nav-link dropdown-toggle" 
+								role="button" 
+								data-bs-hover="dropdown" 
+								aria-expanded="false" 
+								style={{color: "rgb(214, 201, 63)"}}
+							>
+								<h3>FAVORITES</h3>
+							</Link>
+							
+							<ul className="ms-2 dropdown-menu dropdown-menu-dark" style={{color: "rgb(214, 201, 63)"}}>
+								
+								{store.hasFavorites ? data_types.map((data_type) => {
+									return (
+										store.favorites[data_type].map((index) => {
+											return (
+												<li key={uuidv4()} className="d-flex" style={{color: "rgb(214, 201, 63)"}}>
+													<Link to="/detail" className="text-start dropdown-item " style={{color: "rgb(214, 201, 63)"}}>
+														{store.results[data_type][index].name}
+													</Link>
+													<FaTrashAlt 
+														size={15} 
+														title="Delete" 
+														style={{color: "rgb(214, 201, 63)"}} 
+														className="text-end mt-2 me-2" 
+														onClick={() => deleteFavorite(data_type, index)}
+													/>
+												</li>
+											)
+										})
+									)
+								}) : <li className="text-center m-1">No favorites yet</li>}
+							</ul>
+						</li>
+
 					</ul>
 				</div>
 			</div>
-			
 		</nav>
 	);
 };
